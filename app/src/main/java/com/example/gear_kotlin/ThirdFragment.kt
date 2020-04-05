66f2ecb3076729c2925fef5290f9ac6fd659bf88
Mai Huu Hanh <hanhmh1203@gmail.com>
2020-04-05 16:19:34 +0700

    add base in commons

--- app/src/main/java/com/example/gear_kotlin/ThirdFragment.kt
+++ app/src/main/java/com/example/gear_kotlin/ThirdFragment.kt
@@ -3,16 +3,13 @@ package com.example.gear_kotlin
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import com.example.core.UserModel
-import com.example.gear_kotlin.base.BaseFragment
+import com.example.commons.base.BaseFragment
 
 
 class ThirdFragment : BaseFragment() {
